436ad58f22061b656293f53d9bb5b56fed49bb04
ukimiawz <melisa.cecilia@yahoo.com>
2017-12-08 11:49:36 +0100

    Http Response[responseGet] edited by Melisa Cecilia

--- src/main/i5/las2peer/services/uatTestImage/uatTestImage.java
+++ src/main/i5/las2peer/services/uatTestImage/uatTestImage.java
@@ -138,13 +138,25 @@ public class uatTestImage extends RESTService {
   @ApiOperation(value = "getImage", notes = " ")
   public Response getImage() {
 
-    // responseGet
-    boolean responseGet_condition = true;
-    if(responseGet_condition) {
-      JSONObject resultGet = new classes().new image().toJSON();
-      return Response.status(HttpURLConnection.HTTP_OK).entity(resultGet.toJSONString()).build();
+    try { 
+        Connection conn = service.dbm.getConnection();
+        PreparedStatement query = conn.prepareStatement("SELECT * FROM uatTest.tblImage");
+        ResultSet result = query.executeQuery();
+        JSONArray jsonResult = new JSONArray();
+        while(result.next()) {
+          classes.image imageResult = new classes().new image();
+          imageResult.setimageName(result.getString("imageName"));
+          imageResult.setimageUrl(result.getString("imageUrl"));
+          imageResult.setimageId(result.getInt("imageId"));
+          jsonResult.add(imageResult.toJSON());
+        }
+        // responseGetImage
+        return Response.status(HttpURLConnection.HTTP_OK).entity(jsonResult.toJSONString()).build();
+    } catch(Exception e) {
+      e.printStackTrace();
+      JSONObject result = new JSONObject(); 
+      return Response.status(HttpURLConnection.HTTP_INTERNAL_ERROR).entity(result.toJSONString()).build();
     }
-    return null;
   }
 
 
