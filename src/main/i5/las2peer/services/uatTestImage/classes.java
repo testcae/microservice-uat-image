d01500347585136fcadc9f82fdc244b91187128c
ukimiawz <melisa.cecilia@yahoo.com>
2017-12-11 11:08:10 +0100

    Code regeneration/Model synchronization

--- src/main/i5/las2peer/services/uatTestImage/classes.java
+++ src/main/i5/las2peer/services/uatTestImage/classes.java
@@ -6,6 +6,57 @@ import org.json.simple.parser.ParseException;
 
 public class classes {
 
+    class image {
+
+    public image() {}
+    
+    private String imageName;
+
+    public void setimageName(String setValue) {
+        this.imageName = setValue;
+    }
+
+    public String getimageName() {
+        return this.imageName;
+    }
+    private int imageId;
+
+    public void setimageId(int setValue) {
+        this.imageId = setValue;
+    }
+
+    public int getimageId() {
+        return this.imageId;
+    }
+    private String imageUrl;
+
+    public void setimageUrl(String setValue) {
+        this.imageUrl = setValue;
+    }
+
+    public String getimageUrl() {
+        return this.imageUrl;
+    }
+
+    public JSONObject toJSON() {
+
+        JSONObject jo = new JSONObject();
+        jo.put("imageName", this.imageName); 
+        jo.put("imageId", this.imageId); 
+        jo.put("imageUrl", this.imageUrl); 
+
+        return jo;
+    }
+
+    public void fromJSON(String jsonString) throws ParseException {
+        JSONObject jsonObject = (JSONObject) JSONValue.parseWithException(jsonString);
+        this.imageName = (String) jsonObject.get("imageName"); 
+        this.imageId = ((Long) jsonObject.get("imageId")).intValue(); 
+        this.imageUrl = (String) jsonObject.get("imageUrl"); 
+
+    }
+
+}
 
     
 }
