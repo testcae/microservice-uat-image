db10d6ea183c27d5fc3c176257d4ac8f384cb175
ukimiawz <melisa.cecilia@yahoo.com>
2017-12-13 08:14:31 +0100

    Code regeneration/Model synchronization

--- src/main/i5/las2peer/services/uatTestImage/classes.java
+++ src/main/i5/las2peer/services/uatTestImage/classes.java
@@ -6,6 +6,35 @@ import org.json.simple.parser.ParseException;
 
 public class classes {
 
+    class image {
+
+    public image() {}
+    
+    private String imageName;
+
+    public void setimageName(String setValue) {
+        this.imageName = setValue;
+    }
+
+    public String getimageName() {
+        return this.imageName;
+    }
+
+    public JSONObject toJSON() {
+
+        JSONObject jo = new JSONObject();
+        jo.put("imageName", this.imageName); 
+
+        return jo;
+    }
+
+    public void fromJSON(String jsonString) throws ParseException {
+        JSONObject jsonObject = (JSONObject) JSONValue.parseWithException(jsonString);
+        this.imageName = (String) jsonObject.get("imageName"); 
+
+    }
+
+}
 
     
 }
