39c1cfc0b0f031b193de5cc387d7d63f1c962eff
ukimiawz <melisa.cecilia@yahoo.com>
2017-12-13 11:10:55 +0100

    Code regeneration/Model synchronization

--- src/test/i5/las2peer/services/uatTestImage/uatTestImageTest.java
+++ src/test/i5/las2peer/services/uatTestImage/uatTestImageTest.java
@@ -90,6 +90,28 @@ public class uatTestImageTest {
   }
 
 
+  /**
+   * 
+   * Test for the postImage method.
+   * 
+   */
+  @Test
+  public void testpostImage() {
+    MiniClient c = new MiniClient();
+    c.setAddressPort(HTTP_ADDRESS, HTTP_PORT);
+    try {
+      JSONObject payloadPost = new JSONObject();
+      c.setLogin(testAgent.getIdentifier(), testPass);
+      ClientResponse result = c.sendRequest("POST", mainPath + "/post", payloadPost.toJSONString(),
+        MediaType.APPLICATION_JSON, MediaType.APPLICATION_JSON, new HashMap<String,String>());
+      assertTrue(true); // change here
+      System.out.println("Result of 'testpostImage': " + result.getResponse().trim());
+    } catch (Exception e) {
+      e.printStackTrace();
+      fail("Exception: " + e);
+    }
+  }
+
 
 
 
